fe20a0bdb9378aa2aa18f32ac634e326a29dceca
Charlie Giglio <charliegiglio@yahoo.com>
2019-05-01 15:19:50 -0400

    Tests

--- led_button.py
+++ led_button.py
@@ -2,16 +2,16 @@ import RPi.GPIO as GPIO
 
 def button_pushed(event):
         if GPIO.input(18) == 0:
-                GPIO.output(18, GPIO.HIGH)
+                GPIO.output(6, GPIO.HIGH)
         else:
-                GPIO.output(18, GPIO.LOW)
+                GPIO.output(6, GPIO.LOW)
         print("yee")
 
 def setup():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	GPIO.add_event_detect(5, GPIO.RISING, callback=button_pushed)
-	GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(6, GPIO.OUT, initial=GPIO.LOW)
 
 try:
 	setup()
---
+++ servo_test.py
@@ -0,0 +1,8 @@
+import time
+from adafruit_servokit import ServoKit
+
+kit = ServoKit(channels=16, address=65) #i2c address 1x41
+
+kit.servo[0].angle = 180 #Rotate to 180 degrees
+time.sleep(1)
+kit.servo[0].angle = 0 #Rotate to 0 degrees
