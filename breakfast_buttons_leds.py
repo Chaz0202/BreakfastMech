f0c519f93d1abd75f91901e042264f6b0065a609
Charlie Giglio <charliegiglio@yahoo.com>
2019-05-01 20:24:07 -0400

    buttons and led choices working

--- breakfast_buttons_leds.py
+++ breakfast_buttons_leds.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+#GPIO pins of the buttons (in BCM)
 button1 = 5
 button2 = 6
 button3 = 13
@@ -8,11 +9,14 @@ button4 = 19
 button5 = 26
 button6 = 20
 
+#list of all buttons
 button_list = [button1, button2, button3, button4, button5, button6]
 
+#breakfast choices
 choiceA = ""
 choiceB = ""
 
+#GPIO pins of LEDs (in BCM)
 led1 = 27
 led2 = 22
 led3 = 23
@@ -20,49 +24,56 @@ led4 = 24
 led5 = 18
 led6 = 25
 
+#list of all LEDs
 led_list = [led1, led2, led3, led4, led5, led6]
 
-def button1_pushed(event):
+def button1_pushed(event): #to be run when button 1 pushed
 	global choiceA
 	choiceA = "button1"
+	GPIO.output(led2, GPIO.LOW) #turn off other bread choice LED
+	GPIO.output(led1, GPIO.HIGH) #turn on associated LED
 
-def button2_pushed(event):
+def button2_pushed(event): #to be run when button 2 pushed
 	global choiceA
 	choiceA = "button2"
+	GPIO.output(led1, GPIO.LOW) #turn off other bread choice LED
+	GPIO.output(led2, GPIO.HIGH) #turn on associated LED
 
-def button3_pushed(event):
+def button3_pushed(event): #to be run when button 3 pushed
 	global choiceB
 	choiceB = "button3"
+	GPIO.output(led4, GPIO.LOW) #turn off the other spread choice LED
+	GPIO.output(led3, GPIO.HIGH) #turn on associated LED
 
-def button4_pushed(event):
+def button4_pushed(event): #to be run when button 4 pushed
 	global choiceB
 	choiceB = "button4"
+	GPIO.output(led3, GPIO.LOW) #turn off other spread choice LED
+	GPIO.output(led4, GPIO.HIGH) #turn on associated LED
 
+GPIO.setmode(GPIO.BCM) #BCM pin numbering
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(button_list, GPIO.IN, pull_up_down=GPIO.PUD_UP) #setup all buttons as input
 
-GPIO.output(27, GPIO.HIGH)
-
-GPIO.setup(button_list, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+#when button pushed declare an event
 GPIO.add_event_detect(button1, GPIO.RISING)
 GPIO.add_event_detect(button2, GPIO.RISING)
 GPIO.add_event_detect(button3, GPIO.RISING)
 GPIO.add_event_detect(button4, GPIO.RISING)
 GPIO.add_event_detect(button5, GPIO.RISING)
 
+#When event declared run a function
 GPIO.add_event_callback(button1, button1_pushed)
 GPIO.add_event_callback(button2, button2_pushed)
 GPIO.add_event_callback(button3, button3_pushed)
 GPIO.add_event_callback(button4, button4_pushed)
 
-GPIO.setup(led1, GPIO.OUT, initial=GPIO.LOW)
-
-GPIO.output(led1, GPIO.HIGH)
+#setup LEDs as output
+GPIO.setup(led_list, GPIO.OUT, initial=GPIO.LOW)
 
 def main():
-	GPIO.wait_for_edge(button5, GPIO.RISING)
+	#buttons are pushed and variables save choices
+	GPIO.wait_for_edge(button5, GPIO.RISING) #wait until begin button pushed
 	print(choiceA, choiceB)
 
 try:
@@ -70,4 +81,4 @@ try:
 
 finally:
 	print("quitting")
-	GPIO.cleanup()
+	GPIO.cleanup() #remove all pin setups
