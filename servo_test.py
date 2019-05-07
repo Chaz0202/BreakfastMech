3349ce3a5d21d0e43acb953bf06ca1fd00c10d8a
Charlie Giglio <charliegiglio@yahoo.com>
2019-05-06 20:53:52 -0400

    Latest

--- servo_test.py
+++ servo_test.py
@@ -5,4 +5,4 @@ kit = ServoKit(channels=16, address=65) #i2c address 1x41
 
 kit.servo[0].angle = 90 #Rotate to 180 degrees
 time.sleep(1)
-kit.servo[0].angle = 0 #Rotate to 0 degrees
+kit.servo[0].angle = 60 #Rotate to 0 degrees
